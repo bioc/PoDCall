65cf9cb271c6c7887f8d81939eacf733ff028698
Hans Petter Brodal <hanspbr@uio.no>
2026-08-14 09:31:45 +0200

    Error fixes, bumped version to trigger new build

--- NEWS.md
+++
@@ -1,4 +0,0 @@
-# PoDCall 0.99.0
-
-* Submitted the package to CRAN/Bioconductor
-
--- tests/testthat/test_podcallMultiplot.R
+++ tests/testthat/test_podcallMultiplot.R
@@ -4,26 +4,31 @@ test_that("Whether comparison plot is created correctly",{
 
     ## Path to data files
     dataPath <- system.file("extdata", "Amplitudes/", package="PoDCall")
-    amplitudeData <- importAmplitudeData(dataPath, skipLines=0)
+    ampData <- importAmplitudeData(dataPath, skipLines=0)
+    ssPath <- system.file("extdata", "Sample_names.csv", package="PoDCall")
+    sampleSheet <- importSampleSheet(sampleSheet=ssPath, well_id=names(ampData),
+                                     software="QuantaSoft")
 
     ## Select wells and channel to plot
-    wells <- names(amplitudeData)[1:3] # First three wells from set
+    wells <- names(ampData)[1:3] # First three wells from set
 
     ## Get relevant thresholds (see ?thrTable)
     data("thrTable")
     thr <- thrTable[wells, c("thr_target", "thr_ctrl")]
 
     ## Create plot
-    plot <- podcallMultiplot(plateData=amplitudeData[wells],
+    plot <- podcallMultiplot(plateData=ampData[wells],
                              thresholds=thr,
-                             channel="target", colCh=1)
+                             channel="target",
+                             sampleID=sampleSheet$sample_id[1:3],
+                             colCh=1)
 
     ## Does function return a plot
     expect_is(plot, class=c("gg", "ggplot"))
 
     ## Is the number of plotted data points the same as in the data
     nrDatapoints <-
-        sum(vapply(amplitudeData[wells], function(x) nrow(x), numeric(1)))
+        sum(vapply(ampData[wells], function(x) nrow(x), numeric(1)))
     expect_equal(nrow(plot$data), nrDatapoints)
 
 })
